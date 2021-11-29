11a7df383a51cd5aa16d1d8fc71d1521791c3f8c
The Little Fox <77666491+TheLitleFox@users.noreply.github.com>
2021-11-30 00:40:04 +0100

    Update test_uninominal.h

--- test_uninominal.h
+++ test_uninominal.h
@@ -2,6 +2,10 @@
 /// \author Anthony PAILLA
 /// \date november 2021
 
+#ifndef UNI_H
+#define UNI_H
+
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -14,3 +18,5 @@ int min_tab(int *tab, int taille);
 int max_tab(int *tab, int taille, int* max, int excl);
 int uninominale1(MonTableau *tabtab, int* max);
 int uninominale2(MonTableau *tabtab);
+
+#endif
